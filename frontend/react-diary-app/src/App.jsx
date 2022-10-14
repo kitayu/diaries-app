18c42aefbc5214620d9e5dc3455cdbef2dcd41ca
Yoshihisa Kitayuguchi <yoshihisa.kitayuguchi@gmail.com>
2022-10-15 02:40:32 +0900

    ファイルの拡張子をjsxに変更し、不要なファイルを削除

--- frontend/react-diary-app/src/App.jsx
+++ frontend/react-diary-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 import { DiaryList } from './DiaryList';
+import Header from './Header';
 
 function App() {
   return (
     <ChakraProvider>
+      <Header />
       <DiaryList />
     </ChakraProvider>
   );
